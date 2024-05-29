2185fb59043926da9ea20dbdad084cc50029bbb5
PeHenriqueMello <pedrohenriquemellorosa@gmail.com>
2024-05-29 10:49:13 -0300

    area de logado

--- area_logado.php
+++ area_logado.php
@@ -6,6 +6,7 @@
     <title>Área de logado</title>
 </head>
 <body>
+    <!--area de logado com botão para voltar ao formulario -->
     <h1>Seja bem vindo, você está logado!</h1>
 
     <a href="index.php">Sair</a>
